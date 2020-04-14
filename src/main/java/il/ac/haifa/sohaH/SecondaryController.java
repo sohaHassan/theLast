eea9f60b30371077785444e0f33770d595facd31
iMax <iMax@10.0.0.19>
2020-04-14 12:17:49 +0300

    initial commit

---
+++ src/main/java/il/ac/haifa/sohaH/SecondaryController.java
@@ -0,0 +1,12 @@
+package il.ac.haifa.sohaH;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
